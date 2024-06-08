b8974b41cd29a781c23f08037c04e6f473f95d5c
Keira Pierce <kpierce1@samford.edu>
2024-06-08 02:52:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const lowerCaseName = name.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => {
+        return driver.toLowerCase() === lowerCaseName
+    })
+        return matchingDrivers
+    }
+
+    function fuzzyMatch(drivers, name) {
+        const lowerCaseName = name.toLowerCase();
+        const matchingDrivers = drivers.filter(driver => {
+            return driver.toLowerCase().startsWith(lowerCaseName)
+        })
+            return matchingDrivers
+    }
+
+    function matchName(drivers, name) {
+        const lowerCaseName = name.toLowerCase();
+        const matchingDrivers = drivers.filter(driver => {
+            return driver.name.toLowerCase() === lowerCaseName
+        });
+            return matchingDrivers
+    }
